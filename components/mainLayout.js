a47aa9a3a25b53b565e9516cc7ec13ebc02c4c4e
Kevin Davis <dawnpatrol506@gmail.com>
2018-12-08 14:11:03 -0700

    location check in place

--- components/mainLayout.js
+++ components/mainLayout.js
@@ -6,7 +6,7 @@ const MainHead = () => (
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
-        <title>Chum! Login</title>
+        <title>Chum!</title>
     </Head>
 )
 
@@ -47,7 +47,7 @@ class MainLayout extends React.Component {
                 <div className="row">
                     <h1>{this.state.userName}</h1>
                 </div>
-                {this.children}
+                {this.props.children}
                 <script
                     src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
                     integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E="
--- pages/main.js
+++ pages/main.js
@@ -1,9 +1,53 @@
 import MainLayout from '../components/mainLayout';
 
-const Main = () => (
-    <MainLayout>
-        <div>TEST</div>
-    </MainLayout>
-)
+function LocationSwitch(props) {
+    if(props.permission){
+        return(
+            <div className="switch">
+                <label>
+                    Off
+                    <input type="checkbox" />
+                    <span className="lever" />
+                    On
+                </label>
+            </div>
+        )
+    }
+    else{
+        return (
+            <p>You have disabled location services. In order to use chum you must enable them in your browser's settings</p>
+        )
+    }
+}
+
+class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            locationEnabled: true
+        };
+    }
+
+
+    componentDidMount() {
+        navigator.permissions.query({ name: 'geolocation' })
+            .then(pStatus => {
+                console.log(pStatus.state);
+                if (pStatus.state === 'denied') {
+                    this.setState({ locationEnabled: false });
+                }
+            })
+    }
+
+    render() {
+        return (
+            <MainLayout>
+                <div className="row">
+                    <LocationSwitch permission={this.state.locationEnabled} />
+                </div>
+            </MainLayout>
+        )
+    }
+}
 
 export default Main;
